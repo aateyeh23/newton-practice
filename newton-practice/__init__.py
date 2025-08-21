33848099003ef7d9a2c586bc20cff9412e348851
Abdullah Ateyeh <abdullah_ateyeh@berkeley.edu>
2025-08-21 15:21:15 -0700

    Re-reformatting package

---
+++ newton-practice/__init__.py
@@ -0,0 +1,7 @@
+"""
+The Newton-practice package.
+"""
+
+__version__ = "0.0.1"
+
+from .newton import *
---
+++ setup.py
@@ -0,0 +1,6 @@
+# A minimal setup.py file to make a Python project installable.
+
+from setuptools import setup
+
+if __name__ == "__main__":
+	setup()
